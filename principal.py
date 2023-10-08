7f65623c787c491c6bb69edd8ecfb2e7c8fd0259
franAndrad <franciscoezequielandrade@gmail.com>
2023-10-08 02:10:20 -0300

    se terminaron los puntos 1,2,3,4,5 del tp4

--- principal.py
+++ principal.py
@@ -1,18 +1,17 @@
 from procesos import *
 
-
 def menu():
-    print("\nMenu de opciones:")
-    print("0. Salir")
-    print("1. Cargar los datos mediante un archivo CSV")
-    print("2. Cargar nuevo ticket por teclado")
-    print("3. Mostrar todos los datos grabados")
-    print("4. Mostrar registros por patente")
-    print("5. Buscar un código de ticket")
-    print("6. Contar combinaciones entre tipo de vehículo y país de cabina")
-    print("7. Contar por tipo y país")
-    print("8. Calcular distancia promedio y mostrar registros filtrados")
-    return input('Ingrese una opcion: ')
+    opciones = "0. Salir\n"
+    opciones += "1. Cargar los datos mediante un archivo CSV\n"
+    opciones += "2. Cargar nuevo ticket por teclado\n"
+    opciones += "3. Mostrar todos los datos grabados\n"
+    opciones += "4. Mostrar registros por patente\n"
+    opciones += "5. Buscar un código de ticket\n"
+    opciones += "6. Contar combinaciones entre tipo de vehículo y país de cabina\n"
+    opciones += "7. Contar por tipo y país\n"
+    opciones += "8. Calcular distancia promedio y mostrar registros filtrados\n"
+    imprimir_con_cabecera(opciones, 'MENU DE OPCIONES')
+    return input('Ingrese una opción: ')
 
 
 def principal():
@@ -24,15 +23,17 @@ def principal():
         if opcion == '0':
             break
         elif opcion == '1':
-            cargar_datos_desde_csv(DOCUMENTO)
+            cargar_datos_desde_csv(DOCUMENTO, DOCUMENTO_BINARIO)
         elif opcion == '2':
-            cargar_nuevo_ticket(DOCUMENTO)
+            cargar_nuevo_ticket(DOCUMENTO_BINARIO)
         elif opcion == '3':
             mostrar_registros(DOCUMENTO_BINARIO)
         elif opcion == '4':
-            mostrar_registros_por_patente(DOCUMENTO_BINARIO)
+            p = input('\nIngresar la patente a buscar: ')
+            mostrar_registros_por_patente(DOCUMENTO_BINARIO, p)
         elif opcion == '5':
-            buscar_ticket_por_codigo(DOCUMENTO_BINARIO)
+            c = int(input('\nIngresar código de ticket a buscar: '))
+            imprimir_con_cabecera(buscar_ticket_por_codigo(DOCUMENTO_BINARIO, c), 'REGISTRO ENCONTRADO')
         elif opcion == '6':
             matriz_contadora = generar_contador_por_tipo_y_pais(DOCUMENTO_BINARIO)
             mostrar_contador_por_tipo_y_pais(matriz_contadora)
@@ -41,7 +42,7 @@ def principal():
         elif opcion == '8':
             distancia_promedio(DOCUMENTO_BINARIO)
         else:
-            print('Ingrese una opcion valida!')
+            print('¡Ingrese una opción válida!')
 
 
 if __name__ == '__main__':
